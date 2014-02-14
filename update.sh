b0ee621e1ce6e94879a2fed2dd8ef132206f21b3
Stanislaw Pusep <creaktive@gmail.com>
2014-02-14 12:31:53 +0100

    update

--- update.sh
+++ update.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-echo "\n\t* zsh"
-cd ~/.oh-my-zsh
-git pull
+echo "\n * zsh"
+git -C ~/.oh-my-zsh pull
 
-echo "\n\t* vim"
-cd ~/.vim
-git submodule foreach git pull
+echo "\n * vim"
+git -C ~/.vim submodule foreach git pull
 
-echo "\n\t* tmux-powerline"
-cd ~/tmux-powerline
-git pull
+echo "\n * tmux-powerline"
+git -C ~/tmux-powerline pull
 
-echo "\n\t* perl"
-cd ~
+echo "\n * perl"
 perlbrew self-upgrade 2>/dev/null
 cpan-outdated | cpanm
 cpanm-reporter
