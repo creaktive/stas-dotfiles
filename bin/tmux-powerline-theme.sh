c47d187f9e9cc879376e000d1e12c6c04536844d
Stanislaw Pusep <creaktive@gmail.com>
2016-04-20 16:45:14 +0200

    update

--- bin/tmux-powerline-theme.sh
+++ bin/tmux-powerline-theme.sh
@@ -42,12 +42,12 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"earthquake 3 0" \
 		#"pwd 89 211" \
 		#"mailcount 9 255" \
-		"now_playing 234 37" \
+		#"now_playing 234 37" \
 		#"cpu 240 136" \
 		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
 		#"battery 137 127" \
-		"weather 37 255" \
+		#"weather 37 255" \
 		"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
 		"date_day 235 136" \
