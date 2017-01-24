768cf301417842046a5f6d7a6ab059568c725c69
Stanislaw Pusep <creaktive@gmail.com>
2017-01-24 08:29:54 +0100

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+cd $HOME
+mv .git_ .git
+
 echo "\n * zsh"
 git -C ~/.oh-my-zsh pull
 
@@ -8,6 +11,8 @@ git -C ~/.vim submodule foreach git pull
 
 echo "\n * tmux-powerline"
 git -C ~/tmux-powerline pull
+
+mv .git .git_
 
 echo "\n * perl"
 perlbrew self-upgrade 2>/dev/null
